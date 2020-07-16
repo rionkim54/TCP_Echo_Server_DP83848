6354547c755a12a40ad13c8920ed7db7759575d5
ZEROWIN <cherrychance@naver.com>
2020-07-16 18:18:21 +0900

    first release

---
+++ MDK-ARM/TCP_Echo_Client/startup_stm32h743xx.d
@@ -0,0 +1,1 @@
+tcp_echo_client\startup_stm32h743xx.o: startup_stm32h743xx.s
---
+++ MDK-ARM/lan8742.d
@@ -0,0 +1,4 @@
+.\lan8742.o: ../Drivers/BSP/Components/lan8742/lan8742.c
+.\lan8742.o: ../Drivers/BSP/Components/lan8742/lan8742.h
+.\lan8742.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
+.\lan8742.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdio.h
